08c0624b6c63129a27489fc3ead4e830fe2b4377
ShotaroNarita <shotaro.narita@hotmail.com>
2025-09-23 10:54:30 +0900

    feat: refactor SVG generation logic and improve file upload handling

--- src/ui.ts
+++ src/ui.ts
@@ -1,44 +1,40 @@
 import { SvgConfig, Gantt, generateGanttSvg, convert } from "./gantt";
 import { load } from 'js-yaml'
 
-document.getElementById("upload_button")?.addEventListener("change", (event) => {
-    const input = event.target as HTMLInputElement;
-    if (input.files && input.files[0]) {
-        const file = input.files[0];
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const fileContent = e.target?.result;
-            if (typeof fileContent === "string") {
-                const obj = accept_input(fileContent);
-            }
-        };
-        reader.readAsText(file);
-    }
-});
-
-function accept_input(file_str: string){
-    const data = load(file_str) as Gantt;
-    console.log(data);
-    const svg_str = generateGanttSvg(convert(data), config);
-    const svgContainer = document.getElementById("svg_output");
-    if (svgContainer) {
-        svgContainer.innerHTML = svg_str;
-    }
-}
-
-
 let config: Partial<SvgConfig> = {}
 config.width = 1000;
 config.timeAxisFormat = "YYYY/MM";
 config.timeAxisSteps = 12;
 config.customRange = { begin: '2024/1/15', end: '2024/5' };
 
+document.getElementById("upload_button")?.addEventListener("change", (event) => {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+        const file = input.files[0];
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const fileContent = e.target?.result;
+            if (typeof fileContent === "string") {
+                const res = svg_string_from_gantt_string(fileContent);
 
-// const svg_str = generateGanttSvg(convert(eventPlanningGantt));
-// const svg_str = generateGanttSvg(convert(softwareDevelopmentGantt));
+                const svgContainer = document.getElementById("svg_output");
+                if (svgContainer) {
+                    svgContainer.innerHTML = res;
+                }
+            }
+        };
+        reader.readAsText(file);
+    }
+});
 
-// const svg_str = generateGanttSvg(convert(manufacturingGantt), config);
-// const svgContainer = document.getElementById("svg_result");
-// if (svgContainer) {
-//     svgContainer.innerHTML = svg_str;
-// }
+function svg_string_from_gantt_string(file_str: string): string {
+    const data = load(file_str) as Gantt;
+    let result = "";
+    try {
+        result = generateGanttSvg(convert(data), config);
+    } catch (e) {
+        result = `<p style="color:red;">Error: ${(e as Error).message}</p>`;
+    }
+
+    return result;
+}
