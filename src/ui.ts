c751bfd55677f69c875c61c906d5430d38e56d76
ShotaroNarita <shotaro.narita@hotmail.com>
2025-09-23 14:11:59 +0900

    feat: rename file input button for consistency and update event listener logic

--- src/ui.ts
+++ src/ui.ts
@@ -7,34 +7,29 @@ config.timeAxisFormat = "YYYY/MM";
 config.timeAxisSteps = 12;
 config.customRange = { begin: '2024/1/15', end: '2024/5' };
 
-document.getElementById("upload_button")?.addEventListener("change", (event) => {
+let svg_container = document.getElementById("svg_output") as HTMLElement;
+
+document.getElementById("file_input_button")?.addEventListener("change", (event) => {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files[0]) {
         const file = input.files[0];
         const reader = new FileReader();
         reader.onload = function (e) {
-            const fileContent = e.target?.result;
-            if (typeof fileContent === "string") {
-                const res = svg_string_from_gantt_string(fileContent);
-
-                const svgContainer = document.getElementById("svg_output");
-                if (svgContainer) {
-                    svgContainer.innerHTML = res;
-                }
+            const text = e.target?.result;
+            if (typeof text === "string") {
+                const gantt = load(text) as Gantt;
+                // const res = svg_string_from_gantt_string(text);
+                render(svg_container, gantt, config);
             }
         };
         reader.readAsText(file);
     }
 });
 
-function svg_string_from_gantt_string(file_str: string): string {
-    const data = load(file_str) as Gantt;
-    let result = "";
+function render(container: HTMLElement, gantt: Gantt, config: Partial<SvgConfig>) {
     try {
-        result = generateGanttSvg(convert(data), config);
+        container.innerHTML = generateGanttSvg(convert(gantt), config);
     } catch (e) {
-        result = `<p style="color:red;">Error: ${(e as Error).message}</p>`;
+        container.innerHTML = `<p style="color:red;">Error: ${(e as Error).message}</p>`;
     }
-
-    return result;
 }
